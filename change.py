0d0590c7d367c165f5138ec661b26eba294fb958
almagiulianoa <162723354+almagiulianoa@users.noreply.github.com>
2024-03-22 12:56:29 -0300

    Update change.py

--- change.py
+++ change.py
@@ -1,17 +1,17 @@
 def change():
     expense = 23.75
     money = 100
-print (f"Ingresar gasto:")
-expense = 23.75
-print (expense)
-print (f"Dinero recibido:")
-money = 100
-print (money)
-print (f"Vuelto:")
-change = (money-expense)
-print (change)
-print (f"Pesos:")
-print (int (change))
-centavos = (change-int(change))*(int(money))
-print (f"Centavos:")
-print (int(centavos))
+    print (f"Ingresar gasto:")
+    expense = 23.75
+    print (expense)
+    print (f"Dinero recibido:")
+    money = 100
+    print (money)
+    print (f"Vuelto:")
+    change = (money-expense)
+    print (change)
+    print (f"Pesos:")
+    print (int (change))
+    centavos = (change-int(change))*(int(money))
+    print (f"Centavos:")
+    print (int(centavos))
